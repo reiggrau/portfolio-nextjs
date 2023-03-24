133583a529b4d86ba36fcf9f0f2217b044155990
guillemreig <guillemreiggrau@gmail.com>
2023-03-24 15:17:59 +0100

    Added tribes page

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -19,7 +19,7 @@ export default function Navbar() {
     }
 
     function handleShadow() {
-        if (!shadow && window.scrollY >= 90) {
+        if (!shadow && window.scrollY >= 1) {
             setShadow(true);
         } else if (window.scrollY < 1) {
             setShadow(false);
@@ -31,7 +31,7 @@ export default function Navbar() {
     }, []);
 
     return (
-        <div className={shadow ? "fixed w-full h-20 bg-[#ecf0f3] shadow-xl z-[100]" : "fixed w-full h-20 bg-[#ecf0f3] z-[100]"}>
+        <div className={shadow ? "fixed w-full h-20 bg-[#ecf0f3] shadow-xl z-[100] ease-in duration-100" : "fixed w-full h-20 bg-[#ecf0f3] z-[100] ease-in duration-100"}>
             <div className="flex justify-between items-center w-full h-full px-2 2xl:px-16">
                 {/* <Image src={logo} alt="/" width="auto" height="50" /> */}
                 <Link href="/">
@@ -63,7 +63,7 @@ export default function Navbar() {
                 </div>
             </div>
             <div className={nav ? "md:hidden fixed left-0 top-0 w-full h-screen bg-black/70" : ""}>
-                <div className={nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500" : "fixed left-[-100%] top-0 p-10 ease-in duration-500"}>
+                <div className={nav ? "fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500" : "fixed left-[-100%] top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen p-10 ease-in duration-500"}>
                     <div>
                         <div className="flex w-full items-center justify-between">
                             {/* <Image src={logo} alt="/" width="auto" height="50" /> */}
--- pages/tribes.tsx
+++ pages/tribes.tsx
@@ -1,10 +1,43 @@
 import React from "react";
+import Image from "next/image";
+import { RiRadioButtonFill } from "react-icons/ri";
+
+// Assets
+import tribesImg from "./../public/assets/projects/tribes.jpg";
 
 const tribes = () => {
     return (
-        <div className="w-full">
-            <div className="w-screen h-[30vh] lg:w-[40vh] relative">
+        <div className="w-full top-20 relative">
+            <div className="w-screen h-[30vh] lg:h-[40vh] relative">
                 <div className="absolute top-0 left-0 w-full h-[30vh] lg:h-[40vh] bg-black/80 z-10" />
+                <Image className="absolute z-1" fill objectFit="cover" src={tribesImg} alt="/" />
+                <div className="absolute top-[60%] max-w-[1240px] w-full left-[50%] right-[50%] translate-x-[-50%] text-white z-10 p-2">
+                    <h2 className="py-2">TRIBES</h2>
+                    <h3>React / Socket.io / PostgreSQL</h3>
+                </div>
+            </div>
+            <div className="max-w-[1240px] mx-auto p-2 grid md:grid-cols-5 gap-8 pt-8">
+                <div className="col-span-4">
+                    <p>Project</p>
+                    <h2>Overview</h2>
+                    <p>
+                        This is a single-page application website made with React that allows users to sign in and play a 'choose-your-path' browser game. The app also includes a functional lobby where users can find other users, befriend
+                        each other, and chat globally or privately. At the game's prologue the player is asked a few questions, and the answers given determine the stats of the new character.
+                    </p>
+                    <button className="px-8 py-2 mt-4 mr-8">Demo</button>
+                    <button className="px-8 py-2 mt-4">Code</button>
+                </div>
+                <div className="col-span-4 md:col-span-1 shadow-xl shadow-gray-400 rounded-xl p-4">
+                    <div className="p-2">
+                        <p>Technologies</p>
+                        <div>
+                            <p>
+                                <RiRadioButtonFill />
+                                React
+                            </p>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
